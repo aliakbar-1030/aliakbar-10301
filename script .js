9858e589716bb44d9ef581a114dc5891348b7264
aliakbar-1030 <alihdd106@gmail.com>
2025-04-18 13:42:45 +0700

    Add files via upload

---
+++ script .js
@@ -0,0 +1,5 @@
+document.getElementById("startButton").addEventListener("click", function () {
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  document.getElementById("bgMusic").play();
+});
